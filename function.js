af993bce490eb17a66296bfba0b4a7b66e1b3730
Bastián Arce <bestian.design@gmail.com>
2018-02-13 14:28:30 -0300

    First commit adding bio and function

--- function.js
+++ function.js
@@ -16,38 +16,27 @@ var davidBowie = {
     }
   }
 }
+$(document).ready(function() {
+// Datos Personales
+  $('.img-circle').attr('src', davidBowie.datosPersonales.imagen)
+  $('#artist_name').text(davidBowie.datosArtisticos.nombreArtistico)
+  $('#artist_firstname').text(davidBowie.datosPersonales.nombre)
+  $('#artist_lastname').text(davidBowie.datosPersonales.apellido)
+  $('#artist_yearBirthday').text(davidBowie.datosPersonales.nacimiento)
+  $('#artist_country').text(davidBowie.datosPersonales.pais)
+// Datos Artisticos
+  $('#artist_artistName').text(davidBowie.datosArtisticos.nombreArtistico)
+
+  $('#artist_pseudonym ul').empty()
+
+  davidBowie.datosArtisticos.otrosNombres.forEach(function(pseudonimo){
+    $('#artist_pseudonym ul').append('<li>'+ pseudonimo +'</li>')
+  })
 
 
-// Una vez que se encuentre el documento cargado,
-//empezaremos a ubicar cada uno de los datos
-//en los lugares que corresponde
-$(document).ready(function(){
-  // Imagen y título
-  $("#artist_image").attr("src", davidBowie.datosPersonales.imagen);
-  $("#artist_name").html(davidBowie.datosArtisticos.nombreArtistico);
 
-  // Datos Personales
-  $("#artist_firstname").html(davidBowie.datosPersonales.nombre);
-  $("#artist_lastname").html(davidBowie.datosPersonales.apellido);
-  $("#artist_yearBirthday").html(davidBowie.datosPersonales.nacimiento);
-  $("#artist_country").html(davidBowie.datosPersonales.pais);
 
-  //Datos Artísticos
-  $("#artist_artistName").html(davidBowie.datosArtisticos.nombreArtistico);
 
-  // En el caso de los seudónimos (es un arreglo),
-  // debemos limpiar el html de contiene el ul
-  //(quitar esos li que no vamos a utilizar).
-  $("#artist_pseudonym ul").empty();
- // Una vez limpiado el lugar donde vamos a insertar la información,
- //debemos iterar el arreglo de seudónimos,
- //para esto ocupamos forEach, aunque podemos ocupar perfectamente for.
-  davidBowie.datosArtisticos.otrosNombres.forEach(function(nombre){
-    // Durante la iteración,
-    //se ingresa la información dentro del elemento de HTML (ul).
-    // Resumiendo, se crearán cada li con la información necesaria.
-      $("#artist_pseudonym ul").append("<li>" + nombre + "</li>");
-  });
+
+
 });
-
-// De esta forma ingresamos los datos a nuestra página de forma dinámica.
